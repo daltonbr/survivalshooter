1b18b258d3614103f6dfce0201a87153ee49d11a
daltonbr <sacinopatinete@gmail.com>
2015-07-10 14:29:56 -0300

    02 Player Character

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,62 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLenght = 100f;
+
+    void Awake ()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+     
+    void FixedUpdate ()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);         // called these functions every FixedUpdate
+        Turning();
+        Animating(h, v);
+    }
+
+    // Move
+    void Move (float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    // Turning
+    void Turning ()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast (camRay, out floorHit, camRayLenght, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation (newRotation);
+        }
+    }
+
+    // Animating
+    void Animating (float h, float v)
+    {
+        bool walking = ( h != 0f ) || ( v != 0f ) ;
+        if (walking)  Debug.Log(walking); 
+        anim.SetBool ("IsWalking", walking);
+    }
 }
